4210524fc12094a2e9b47b8fbfed82e9c86336af
saitoumizuki <yoshihiramizuki@gmail.com>
2018-05-18 15:10:52 +0900

    Finish 5-4

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-
-  has_many :post_images
-  
+# Include default devise modules. Others available are:
+# :confirmable, :lockable, :timeoutable and :omniauthable
+	devise :database_authenticatable, :registerable,
+		:recoverable, :rememberable, :trackable, :validatable
+	has_many :post_images
+	has_many :post_comments, dependent: :destroy
+	has_many :favorites, dependent: :destroy
 end
