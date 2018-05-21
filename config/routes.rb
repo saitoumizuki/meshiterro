2622beb6faf784a5ca8f090d05e4cf43d9f95d20
saitoumizuki <yoshihiramizuki@gmail.com>
2018-05-21 10:26:36 +0900

    Finish 5-6

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
+  # get 'users/edit' => 'users#edit'
+
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'post_images#index'
-  resources :post_images, only: [:new, :create, :index, :show] do
+  resources :users
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
   	resource :favorites, only: [:create, :destroy]
-    resource :post_comments, only: [:create, :destroy]
-	end
+  	resource :post_comments, only: [:create, :destroy]
+  end
+
+  root 'post_images#index'
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
